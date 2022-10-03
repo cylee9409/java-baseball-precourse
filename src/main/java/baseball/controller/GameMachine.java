86426316539b029e024fd131254d5e818ec13d23
cylee9409 <67910616+cylee9409@users.noreply.github.com>
2022-10-03 15:35:12 +0900

    feat : playBaseballGame() 메서드 구현
    - BaseballGameService를 생성하여 실제 Main에서 호출할 playBaseballGame() 메서드 구현

---
+++ src/main/java/baseball/controller/GameMachine.java
@@ -0,0 +1,31 @@
+package baseball.controller;
+
+import baseball.service.BaseballGameService;
+import baseball.view.ConsolePrinter;
+import baseball.view.Messages;
+import baseball.view.ConsoleScanner;
+
+public class GameMachine {
+    private BaseballGameService baseBallGameService;
+    private static final String gameButtonPattern = "^[1-2]*$";
+    private static final String REPLAY_NUM = "1";
+
+    public void playBaseballGame() {
+
+        String gameReplayButton = new String();
+        do {
+            baseBallGameService = new BaseballGameService();
+            baseBallGameService.playGame();
+            ConsolePrinter.printConsoleMsg(Messages.ASK_REPLAY.getMsg());
+            gameReplayButton = ConsoleScanner.getUserInput();
+            validateUserInput(gameReplayButton);
+        } while (REPLAY_NUM.equals(gameReplayButton));
+    }
+
+    public void validateUserInput(String gameReplayButton) {
+        if (!gameReplayButton.matches(gameButtonPattern)) {
+            System.out.println("게임 버튼 값 오류");
+            throw new IllegalArgumentException();
+        }
+    }
+}
